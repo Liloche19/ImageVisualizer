f0c1f9938f54063ae60a916af3865843d68acd39
Liloche19 <lilian.locher@epitech.eu>
2025-07-01 14:47:29 +0200

    [+] C project initialised + terminal size and ratio obtained

---
+++ include/visualizer.h
@@ -0,0 +1,14 @@
+#ifndef _VISUALIZER_H_
+    #define _VISUALIZER_H_
+
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <string.h>
+    #include <sys/ioctl.h>
+
+    #define RESET "\033[0m"
+    #define CHAR " "
+    #define CHAR_RATIO 2.29 / 1
+
+#endif //_VISUALIZER_H_
